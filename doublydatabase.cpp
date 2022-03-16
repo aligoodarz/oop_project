6b2573484b437640449817700b92d762aacbe6f6
aligoodarz <72888089+aligoodarz@users.noreply.github.com>
2022-03-16 13:40:00 -0400

    Added some definitions for NodeList

--- doublydatabase.cpp
+++ doublydatabase.cpp
@@ -1,5 +1,7 @@
 #include "doublydatabase.h"
 
+// The following are function defenitions for the iterator
+
 NodeList::Iterator::Iterator(DoublyNode *u) { v = u; }
 
 Elem &NodeList::Iterator::operator*() { return v->elem; }
@@ -18,4 +20,25 @@ NodeList::Iterator &NodeList::Iterator::operator--()
 {
     v = v->prev;
     return *this;
+}
+
+// The following are the function definitions for the list itself
+
+NodeList::NodeList()
+{
+    n = 0; // Initialize size to zero
+    // Create the sentinel nodes and point them to each other
+    header = new DoublyNode;
+    trailer = new DoublyNode;
+    header->next = trailer;
+    trailer->prev = header;
+}
+
+int NodeList::size() const { return n; }
+
+bool NodeList::empty() const { return (n == 0); }
+
+NodeList::Iterator NodeList::begin() const
+{
+    return Iterator(header->next);
 }
