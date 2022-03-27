04ebb2c505ca634ef6dba0ffe5d7eb5797fc9eec
aligoodarz <72888089+aligoodarz@users.noreply.github.com>
2022-03-27 16:02:14 -0400

    Completed declarations in tree.h

--- tree.h
+++ tree.h
@@ -1,5 +1,7 @@
+#include <list>
+
 typedef int Elem;
-class LinkedBinaryList
+class LinkedBinaryTree
 {
 protected:
     struct Node
@@ -19,5 +21,48 @@ public:
 
     public:
         Position(Node *_v = nullptr) : v(_v) {} // constructor
-    }
+        Elem &operator*()
+        {
+            return v->elt;
+        }
+        Position left() const
+        {
+            return Position(v->left);
+        }
+        Position right() const
+        {
+            return Position(v->right);
+        }
+        Position parent() const
+        {
+            return Position(v->par);
+        }
+        bool isRoot() const
+        {
+            return v->par == nullptr;
+        }
+        bool isExternal() const
+        {
+            return v->left == nullptr && v->right == nullptr;
+        }
+        friend class LinkedBinaryTree;
+    };
+    typedef std::list<Position> PositionList;
+
+public:
+    LinkedBinaryTree(); // Constructor
+    int size() const;
+    bool empty() const;
+    Position root() const;          // Get the root
+    PositionList positions() const; // List of nodes
+    void addRoot();                 // Add root to empty tree
+    void expandExternal(const Position &p);
+    Position removeAboveExternal(const Position &p);
+
+protected:
+    void preorder(Node *v, PositionList &pl) const;
+
+private:
+    Node *_root;
+    int n;
 };
