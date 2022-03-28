12306ec7161d79239525c2d059439fcd1fa78d3e
aligoodarz <72888089+aligoodarz@users.noreply.github.com>
2022-03-28 12:22:15 -0400

    Added pre order

--- tree.cpp
+++ tree.cpp
@@ -22,3 +22,45 @@ void LinkedBinaryTree::expandExternal(const Position &p)
     v->right->par = v;
     n += 2;
 }
+
+LinkedBinaryTree::Position
+LinkedBinaryTree::removeAboveExternal(const Position &p)
+{
+    Node *w = p.v;                                   // Get node at position p
+    Node *v = w->par;                                // Get the parent of node w
+    Node *sib = (w == v->left ? v->right : v->left); // Set the sibling of w
+    if (v == _root)
+    {
+        _root = sib;
+        sib->par = nullptr;
+    }
+    else
+    {
+        Node *gpar = v->par; // Get w's grandparent
+        if (v == gpar->left)
+            gpar->left = sib;
+        else
+            gpar->right = sib;
+        sib->par = gpar;
+    }
+    delete w;
+    delete v;
+    n -= 2;
+    return Position(sib);
+}
+
+void LinkedBinaryTree::preorder(Node *v, PositionList &pl) const
+{
+    pl.push_back(Position(v));
+    if (v->left != nullptr)
+        preorder(v->left, pl);
+    if (v->right != nullptr)
+        preorder(v->right, pl);
+}
+
+LinkedBinaryTree::PositionList LinkedBinaryTree::positions() const
+{
+    PositionList pl;
+    preorder(_root, pl);
+    return PositionList(pl);
+}
